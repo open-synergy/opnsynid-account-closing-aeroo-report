be3b4f901966ddc76b116c42d5f29e74a195fdbd
Andhitia Rama <andhitia.r@gmail.com>
2022-04-14 13:13:03 +0700

    [account_amortization_common_aeroo_report] 8.0.1.0.0

---
+++ account_amortization_common_aeroo_report/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Amortization Common Aeroo Report",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "account_amortization_common",
+        "report_aeroo",
+    ],
+    "data": [
+        "reports/report_account_amortization_ods.xml",
+        "reports/report_account_amortization_xls.xml",
+        "wizards/wizard_account_amortization.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "license": "AGPL-3",
+}
